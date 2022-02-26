9de70298d5dc6944ec8fae290b5550f7a1650e1f
shooshanaa <zane@kruming.lv>
2022-02-26 17:33:11 +0200

    hi

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello world!");
+    int number;
+    number = 4;
+    int value = 6;
+    System.out.println("Addition of number and value is " + (value+number) );
+    System.out.println("Subtraction of number and value is " + (value-number) );
+    System.out.println("Multiplication of number and value is " + (value*number) );
+    System.out.println("Division of number and value is " + (value/number) );
+    System.out.println("Modulus of number and value is " + (value%number) );
+
+    }
+}
