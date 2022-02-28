029ff2937caf4f02698e17bb2b7681f48b8fa9f4
shooshanaa <zane@kruming.lv>
2022-02-28 21:21:04 +0200

    hi

---
+++ src/com/company/Conditionals.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        int number;
+        number = 10;
+        if (number >= 9) {
+            System.out.println(number);
+        } else {
+            System.out.println("not bigger than 9");
+        }
+
+    }
+
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,5 +13,30 @@ public class Main {
     System.out.println("Division of number and value is " + (value/number) );
     System.out.println("Modulus of number and value is " + (value%number) );
 
+
+//   users age, gender, heigth, weigth,
+
+        int age = 26;
+        System.out.println("Age is " + (age));
+
+        char gender = 'F';
+        System.out.println("Gender: " + (gender));
+
+        double heigth;
+        heigth = 1.65;
+        System.out.println("Heigth is " + (heigth) + "m");
+
+        int weigth;
+        weigth = 52;
+        System.out.println("Weigth is " + (weigth) + "kg");
+
+
+
+        System.out.println(6<9 && 9>2);
+        System.out.println(5 > 9 || 8 < 3);
+        System.out.println(8==2 && 7<6);
+
+
+
     }
 }
