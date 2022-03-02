7b2737efe6bc5403d2f1785246fcfc88fbac449b
shooshanaa <zane@kruming.lv>
2022-03-02 20:19:38 +0200

    hi

--- src/com/company/Conditionals.java
+++ src/com/company/Conditionals.java
@@ -2,13 +2,44 @@ package com.company;
 
 public class Conditionals {
     public static void main(String[] args) {
-        int number;
-        number = 10;
-        if (number >= 9) {
-            System.out.println(number);
+        int value;
+        value = 9;
+        if (value > 9 && value <15) {
+            System.out.println(value);
         } else {
             System.out.println("not bigger than 9");
         }
+        // Task: 1
+
+        int count = 1;
+        count = count +1;
+
+        for (int i =1; i<51; i++ ) {
+            if (i % 2 == 0) {
+                System.out.println(i + " is even");
+            } else {
+                System.out.println(i + " is odd");
+            }
+        }
+
+         //Task:2
+
+            int counter = 1;
+
+
+         while (counter <= 50) {
+            if (counter %4 == 0) {
+                System.out.println(counter + " is a multiple of 4");
+
+            }
+            counter ++;
+
+            }
+        }
+
+
+
+
 
     }
 
