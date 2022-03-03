c2fc179724b1c2e71312e34a06b44dcb9bf9a1a3
shooshanaa <zane@kruming.lv>
2022-03-03 18:54:18 +0200

    homework

---
+++ src/com/company/homework_1.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class homework_1 {
+    public static void main(String[] args) {
+        System.out.println("Task 1:");
+        int number = 1;
+        number = number +1;
+
+        for (int i = 2; i <80 ; i++) {
+            if( i%i==0 && i%1==0) {
+                System.out.println(i + " is a prime number");
+            }
+        }
+    }
+}
