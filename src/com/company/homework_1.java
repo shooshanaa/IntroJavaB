bea07817c2f3255495fa5b1aecaf5081c290081c
shooshanaa <zane@kruming.lv>
2022-03-04 19:44:40 +0200

    homework = done

--- src/com/company/homework_1.java
+++ src/com/company/homework_1.java
@@ -2,14 +2,45 @@ package com.company;
 
 public class homework_1 {
     public static void main(String[] args) {
-        System.out.println("Task 1:");
-        int number = 1;
-        number = number +1;
+        // Write a program that will output the prime numbers between 1- 80.
+        System.out.println("Task 1: ");
 
-        for (int i = 2; i <80 ; i++) {
-            if( i%i==0 && i%1==0) {
-                System.out.println(i + " is a prime number");
+        for (int i = 1; i <= 80; i++) {
+            int counter = 0;
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    counter++;
+                }
             }
+
+            if (counter == 0) {
+                System.out.println(i + " is prime");
+
+            }
+
         }
+        //Write a program that will act as a simple calculator where you set a variable as the operator and based on that
+        //perform an arithmetic operation. For example if the operator variable is "+" then it will add two numbers  and so on
+
+        System.out.println("Task 2:");
+
+        char operator = '*';
+        int bigNumber = 15;
+        int smallNumber = 3;
+        if (operator == '+') {
+
+            System.out.println("The sum is " + (bigNumber + smallNumber));
+
+        } else if (operator == '-') {
+            System.out.println("The difference is " + (bigNumber - smallNumber));
+        } else if (operator == '*') {
+            System.out.println("The product is " + (bigNumber * smallNumber));
+        } else
+            System.out.println("Operator not recognized");
+
+
+
     }
+
+
 }
