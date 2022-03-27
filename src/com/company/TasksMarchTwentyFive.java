f168d7a3dd7fa9c5c5a8427358df383b0e7c3cb5
shooshanaa <zane@kruming.lv>
2022-03-27 22:50:10 +0300

    home assignments - shopping list, magic dates etc.

--- src/com/company/TasksMarchTwentyFive.java
+++ src/com/company/TasksMarchTwentyFive.java
@@ -1,4 +1,7 @@
 package com.company;
+import javafx.util.converter.LocalDateStringConverter;
+
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 import java.util.Arrays;
 import java.time.LocalDate;
@@ -8,7 +11,7 @@ public class TasksMarchTwentyFive {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-     /*   //Task 1: BMI for 5 persons based on heigth and weigth
+       /*   //Task 1: BMI for 5 persons based on heigth and weigth
 
         System.out.println("Enter your weigth : ");
         double weigthOne = scanner.nextDouble();
@@ -50,64 +53,107 @@ public class TasksMarchTwentyFive {
 
         System.out.println("The average bmi of the group is " + average);*/
 
-        //Task 2: the most used charachter of a string
+        //Task 2: the most used character of a string
+        //Given a string from the user return the character and count of the character that occurs the most
+        //in the string.
 
-        System.out.println("Please enter a sentence: ");
+     /*   System.out.println("Please enter a sentence: ");
         String userInput = scanner.nextLine();
-        int[] freqw = new int[userInput.length()];
 
-        char[] userString = userInput.toCharArray();
+        String mostUsedChar = "";
+        int count = 0;
 
-        for (int i = 0; i < userInput.length(); i++) {
-            freqw[i] = 1;
-            for (int j = i+1; j < userInput.length(); j++) {
-                if(userString[i]== userString[j]) {
-                    freqw[i] ++;
-                    userString[j]=0;
+        String[] array = userInput.split("");
+        for (int i = 0; i < array.length; i++) {
+            int temp = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[i].equals(array[j])) {
+                    temp++;
                 }
-                
-            }
-            }
-        System.out.println("Charachter frequencies: ");
-        for (int i = 0; i < freqw.length; i++) {
-            if(userString[i]!=' ' && userString[i]!=0) {
-                System.out.println(userString[i] + " " + freqw[i]);
-            }
+                if (temp > count) {
+                    count = temp;
+                    mostUsedChar = array[i];
 
+                    }
+                }
+
+            }
+            System.out.println("The most used character is " + mostUsedChar + " used " + count + " times.");*/
+
+
+        //Task 3:
+        // You're sent to a grocery store with the following items on your list ["Bananas", "Bread", "Fish"]
+        // The cost of the items in the store is [12, 5, 10], your current cash-in-hand is 20 dollars. Which
+        // of the items can you safely buy without needing extra cash.
+        // Priority - Bananas[least], Fish[second], Bread[First]
+        // N.B. you can't buy half of an item.
+        // You can buy -- and -- and you'll be left with -- dollars.
+
+     /*   String[] list = {"bananas", "bread", "fish"};
+        int bananas = 12;
+        int bread = 5;
+        int fish = 10;
+        int cash = 20;
+
+        System.out.println(Arrays.toString(list));
+
+        int[] price = {12, 5, 10};
+
+        int sum = 0;
+        int count = 0;
+
+        Arrays.sort(price);
+        System.out.println(Arrays.toString(price));
+
+        for (int i = 0; i < price.length; i++) {
+            if (sum + price[i] <= cash) {
+
+                sum += price[i];
+                count++;
+            }
         }
+        System.out.println("You can buy " + count + " items and you are left with " + (cash - sum) + " dollars.");*/
 
 
-       /* // Task 4: magic dates
+
+        // Task 4: magic dates
+       // Receive the year, month and day from the user separately.
+        // - The year should be a four-digit number.
+        // - Assume that the date is correct
+        // Determine if the date is a magic date
+        // A magic date occurs when:
+        // - m * d  is a 1-digit number that matches the last digit of the year. OR
+        // - m * d is a 2-digit number that matches the last 2 digits of the year. OR
+        // - m * d is a 3-digit number that matches the last 3 digits of the year.
+
+        // For example, 1 2 2012
+
+        // return the output interactively to the user.
 
         Date date = new Date();
 
-        System.out.println("Enter a year ");
-        int year = scanner.nextInt();
 
-        System.out.println("Enter a month ");
-        int month = scanner.nextInt();
+            System.out.println("Enter a year ");
+            String year = scanner.nextLine();
 
-        System.out.println("Enter a day ");
-        int day = scanner.nextInt();
+            System.out.println("Enter a month ");
+            String month = scanner.nextLine();
 
-        String dateinString;
+            System.out.println("Enter a day ");
+            String day = scanner.nextLine();
 
+            int y = Integer.parseInt(year);
+            int m = Integer.parseInt(month);
+            int d = Integer.parseInt(day);
 
-        String [] userDate = {"year", "month", "day"};
-       System.out.println(date.toString());*/
-
-      /*  for (int i = 0; i < userDate.length; i++) {
-            if (userDate[1]*userDate[2]==userDate[0]) {
-                System.out.println("You entered a magic date" + userDate);
-            }
-
-        }*/
+            String [] yArray = year.split("");
 
 
+           if ((m*d)==y) {
+                System.out.println( " is a magic date.");
 
-
-
-
+            } else
+                System.out.println("nothing magical about this date.");
 
 
 
